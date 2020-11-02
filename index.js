c21b7a926ed69f4155ec801b4a97ee71aadca2c4
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-02 16:25:52 -0500

    Updated questions, wrote init function and writeToFile function, and added required dependencies.

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 // Dependencies
-var inquirer = require("inquirer");
-var fs = require("fs");
-var createReadMe = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
   {
     type: "input",
-    message: "What is your project title?",
+    message: "What is your project's title?",
     name: "title",
   },
   {
     type: "input",
-    message: "What is the description for your project?",
+    message: "Please write a short description for your project:",
     name: "description",
   },
   {
     type: "input",
-    message: "What are the steps required for installation?",
+    message: "What command should be run to install dependencies?",
     name: "install",
+    default: "npm i",
   },
   {
     type: "input",
-    message: "Explain how a user can use your project.",
+    message: "What command should be run to run tests?",
+    name: "testing",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about using the repo?",
     name: "usage",
   },
   {
     type: "input",
-    message: "What are your contribution guidelines?",
+    message: "What does the user need to know about contributing to the repo?",
     name: "contrib",
   },
   {
-    type: "input",
-    message: "What are the testing instructions?",
-    name: "testing",
-  },
-  {
     type: "list",
-    message: "Which license should your project have?",
+    message: "What kind of license should your project have?",
     name: "license",
-    choices: ["GNU v3.0", "Apache License 2.0", "ISC", "MIT"],
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
   },
   {
     type: "input",
@@ -48,16 +51,37 @@ const questions = [
   },
   {
     type: "input",
-    message: "What is your preferred email address?",
+    message: "What is your email address?",
     name: "email",
   },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("Success! Your README file has been created.");
+    }
+  });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
 
 // function to initialize program
-function init() {}
+async function init() {
+  try {
+    const data = await inquirer.prompt(questions);
+    console.log(data);
+
+    const readMe = generateMarkdown(data);
+
+    await writeFileAsync("sampleREADME.md", readMe);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // function call to initialize program
 init();
