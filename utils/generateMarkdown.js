e02f76d42007dc63af490eaee9120d113aa1091f
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-02 17:00:01 -0500

    Created generateMarkdown function basic layout.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,52 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let licenseBadge = ``;
+  if (data.license === "None") {
+    licenseBadge = ``;
+  } else if (data.license === "MIT") {
+    licenseBadge = `# [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (data.license === "APACHE 2.0") {
+    licenseBadge = `# [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (data.license === "GPL 3.0") {
+    licenseBadge = `# [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (data.license === "BSD 3") {
+    licenseBadge = `# [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  }
+
   return `
   # ${data.title}
+  
+  ${licenseBadge}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.contrib}
+
+  ## Tests
+  ${data.testing}
+
+  ## Questions
+  ${data.username}
+  ${data.email}
 `;
 }
 
